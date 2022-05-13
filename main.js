a182c882c246a95be2a95088fceaa0fcb97987d7
Cderoslin <cderoslin@hotmail.com>
2022-05-13 16:06:10 -0400

    Update

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 let localStream;
 let remoteStream;
+let peerConnection;
+
+const servers = {
+    iceServers:[
+        {
+            urls:[]
+        }
+    ]
+}
 
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
     document.getElementById('user-1').srcObject = localStream
+
+    createOffer()
+}
+
+let createOffer = async () => {
+    peerConnection = new RTCPeerConnection()
+
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
+
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
+
+
 }
 
 init()
